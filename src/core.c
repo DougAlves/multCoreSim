8ff0bd7cf3de5634052378175202b9e31b1c67eb
Douglas Lima <douglas.felipelima18@gmail.com>
2019-11-07 10:49:36 -0300

    compilando pelo menos

--- src/core.c
+++ src/core.c
@@ -1,24 +1,20 @@
 #include "../include/core.h"
 #include <stdlib.h>
 
-typedef struct Core {
 
-	Thread t1;
-
-	Thread t2;
-
-	int * l2;
-	
-} Core;
-
-Core*
+CPU
 createProcessor(int cCores)
 {
 	Core* cores = malloc(sizeof(Core)*cCores);
 	for(int i = 0; i< cCores; ++i){
-		cores[i].t1 = malloc(sizeof(int)*liSize);
-		cores[i].t2 = malloc(sizeof(int)*liSize);
+		cores[i].t1 = malloc(sizeof(int)*l1Size);
+		cores[i].t2 = malloc(sizeof(int)*l1Size);
 		cores[i].l2 = malloc(sizeof(int)*l2Size);
 	}
 	return cores;
+}
+
+void
+write( Core* cores, int core, int addr, int info ){
+	
 }
